ce2db175a934c8e951be7316e560d2ca49678fd2
sppr0483-lang <sppr0483@gmail.com>
2026-08-12 14:43:36 -0600

    Update 002_Riesgo_compania.R

--- Procesos/002_Riesgo_compania.R
+++ Procesos/002_Riesgo_compania.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(readr)
 library(readxl)
-source("Funciones_EBR.R")
+source("Funciones_principales.R")
 #####Se carga la plantilla proporcionada por las áreas relacionadas 
 #CarteraSegurosSimulada <- read_csv("C:/Users/agarciadeleon/WPy64-38123/scripts/EBR/CarteraSegurosSimulada1.csv")
 CarteraSegurosSimulada <- read_csv("C:/Users/agarciadeleon/WPy64-38123/scripts/EBR/Layout_ok1.csv", 
